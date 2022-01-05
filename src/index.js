606e3edb4dd9f2c48b0161ad183d8cccd150524d
evelinrome278 <evelin.romero84@gmail.com>
2022-01-04 23:07:15 -0300

    creacion home, config firebase, crear tweet

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import ProtectedContext from './Context/Protecter';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ProtectedContext>
+
+        <App />
+      </ProtectedContext>
+  </BrowserRouter>    
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <h1>Dev United</h1>
+  );
+};
+
+export default Home;
